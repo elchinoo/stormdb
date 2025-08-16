1d249f0d3a8c0b6a3b0760cdd7830601e6672c81
Charly Batista <charly.batista@enterprisedb.com>
2025-08-17 06:21:39 +0800

    ci: export POSTGRES_TEST_* env vars when running tests in compose

--- scripts/ci/run_in_compose.sh
+++ scripts/ci/run_in_compose.sh
@@ -7,5 +7,13 @@ ROOT=$(cd "$(dirname "$0")/../.." && pwd)
 cd "$ROOT"
 # Ensure compose services are up (will start stormdb-dev and postgres-test if not present)
 docker compose up -d ${SERVICE} postgres-test
+# Environment variables for tests to connect to postgres-test service
+POSTGRES_TEST_HOST=${POSTGRES_TEST_HOST:-postgres-test}
+POSTGRES_TEST_PORT=${POSTGRES_TEST_PORT:-5432}
+POSTGRES_TEST_DB=${POSTGRES_TEST_DB:-stormdb_test}
+POSTGRES_TEST_USER=${POSTGRES_TEST_USER:-stormdb}
+POSTGRES_TEST_PASSWORD=${POSTGRES_TEST_PASSWORD:-stormdb_test}
+
 # Build inside container and run all tests
-docker compose exec -T ${SERVICE} /bin/bash -lc "cd /workspace && make run-tests"
+docker compose exec -T ${SERVICE} /bin/bash -lc "cd /workspace && \
+  export POSTGRES_TEST_HOST=${POSTGRES_TEST_HOST} POSTGRES_TEST_PORT=${POSTGRES_TEST_PORT} POSTGRES_TEST_DB=${POSTGRES_TEST_DB} POSTGRES_TEST_USER=${POSTGRES_TEST_USER} POSTGRES_TEST_PASSWORD=${POSTGRES_TEST_PASSWORD} && make run-tests"
